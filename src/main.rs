06284b53540d4ee2203ac7a74a35bb85cbf7b234
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2020-05-19 14:13:46 +0900

    return type to Option<String>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 const VRC_LOG_LOCATION:  &str = "\\AppData\\LocalLow\\VRChat\\VRChat";
 
 fn main() {
-
-    print!("{}", vrc_log_path());
+    match vrc_log_path() {
+        None => println!("Log file not found"),
+        Some(s) => println!("log file {}", &s),
+    }
 }
 
-fn vrc_log_path() -> String {
+fn vrc_log_path() -> Option<String> {
      if let Some(v) = dirs::home_dir() {
          if let Some(v) = v.to_str() {
-             return v.to_string() +  VRC_LOG_LOCATION;
+             return Some(v.to_string() +  VRC_LOG_LOCATION);
          }
      }
 
-     "".to_string()
+     None
 }
