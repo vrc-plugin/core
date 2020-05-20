ed1292975ff6877eab42c7c7132f6c4aecb8b098
bootjp <oh@bootjp.me>
2020-05-20 15:57:45 +0900

    add wip vrc_latest_log

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-const VRC_LOG_LOCATION:  &str = "\\AppData\\LocalLow\\VRChat\\VRChat";
+extern crate glob;
+
+const VRC_LOG_LOCATION: &str = "\\AppData\\LocalLow\\VRChat\\VRChat\\";
+
+use self::glob::glob;
+use std::fs::{read_dir, ReadDir, metadata, DirEntry};
+use std::{fs, io};
+use std::path::PathBuf;
 
 fn main() {
-    match vrc_log_path() {
-        None => println!("Log file not found"),
-        Some(s) => println!("log file {}", &s),
+    let mut path = vrc_log_path().expect("user directory not detected");
+    println!("log file {}", &path);
+    for x in vrc_latest_log(path) {
+        for y in x {
+            println!("{:?}", y)
+        }
     }
 }
 
 fn vrc_log_path() -> Option<String> {
-     if let Some(v) = dirs::home_dir() {
-         if let Some(v) = v.to_str() {
-             return Some(v.to_string() +  VRC_LOG_LOCATION);
-         }
-     }
+    if let Some(v) = dirs::home_dir() {
+        if let Some(v) = v.to_str() {
+            return Some(v.to_string() + VRC_LOG_LOCATION);
+        }
+    }
 
-     None
+    None
+}
+
+fn vrc_latest_log(dir_path: String) -> Result<Vec<PathBuf>, io::Error> {
+    Ok(fs::read_dir(dir_path)?
+        .into_iter()
+        .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
+        .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
+        .filter(|r| r.file_name().unwrap().to_str().unwrap().contains("output")) // Filter out non-folders
+        .collect())
 }
